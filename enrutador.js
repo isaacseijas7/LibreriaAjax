e636a83dbe6b3924c15c5e23d89462b55232df3b
isaacseijas7 <isaacseijas7@gmail.com>
2016-10-10 16:59:24 -0400

    Libreria para enrutar vistas con ajax

---
+++ enrutador.js
@@ -0,0 +1,63 @@
+(function ( w, d, n, xhr ) {
+	
+	'use strict';
+
+	class Enrutador{
+		
+		constructor( marco ){
+
+			this.marco = marco;
+			this.rutas = {};
+
+		}
+
+		addRuta( ruta, plantilla, controlador, cargar ){
+			
+			this.rutas[ruta] = {
+				plantilla: plantilla, 
+				controlador: controlador, 
+				cargar: cargar
+			}
+
+		}
+
+		manejadorRutas(){
+			let hash = w.location.hash.substring(1) || '/',
+				destino = this.rutas[hash],
+				self = this;
+
+			if (destino && destino.plantilla) {
+				xhr.ejecutar({
+					type: 'GET',
+					url: destino.plantilla,
+					loading: function(){
+						self.marco.innerHTML = 'Cargando...';
+					},
+					success: function(datos){
+						self.marco.innerHTML = datos;
+
+						if (typeof self.rutas[hash].cargar=== 'function' ) {
+							self.rutas[hash].cargar();	
+						}
+
+					}
+				});
+			}else {
+				w.location.hash = '#/';
+			}
+
+		}
+
+	}
+
+	var marco = d.querySelector('#vistas');	
+
+	if (typeof w.enrutador==='undefined') {
+		w.enrutador = new Enrutador(marco);
+		w.addEventListener('load', enrutador.manejadorRutas.bind(enrutador), false);
+		w.addEventListener('hashchange', enrutador.manejadorRutas.bind(enrutador), false);
+	}else {
+		console.log('Ya existe la librería ajax');
+	}
+
+})( window, document, navigator, ajax );
